e2c807a01cea9c0b29897aeb9e66fc153f437a6e
FrankJlS <coneronal-08@hotmail.com>
2022-11-01 20:08:44 -0500

    Pull request Multiplicacion

---
+++ Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	private int num1;
+	private int num2;
+
+	public Calculadora()
+	{
+	}
+
+	public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+	public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+	public int multiplicacion()
+	{
+		return num1 * num2;
+	}
+
+}
