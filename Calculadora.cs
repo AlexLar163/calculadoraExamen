ac946eb15dbc505057e39246d90301c1f8221d2f
0501jmn <merchanjhanny05@gmail.com>
2022-11-01 20:08:10 -0500

    Agregue la realizacion de la suma

---
+++ Calculadora.cs
@@ -0,0 +1,18 @@
+﻿public class Class1
+{
+    private int num1;
+    private int num2;
+
+    public Calculadora()
+    {
+    }
+
+    public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+    public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+    public int Suma()
+    {
+        return num1 + num2;
+    }
+
+}
